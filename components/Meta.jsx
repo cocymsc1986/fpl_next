fb14b2642d22c0266b46f505b6916a33dd95a614
Michael Cockcroft <cocymsc@hotmail.com>
2018-11-15 20:15:41 +0000

    Revert "Adding GA script"

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -4,15 +4,6 @@ import theme from '../styles/theme';
 export default () => (
   <div>
     <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129266648-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-129266648-1');
-      </script>
-
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta charSet="utf-8" />
 			<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet"></link>
