90421b77dcd43c8f589f221ca92048cd4d3ab7c5
Michael Cockcroft <cocymsc@hotmail.com>
2019-07-26 23:09:53 +0100

    adding ga code for page views

--- layout/index.js
+++ layout/index.js
@@ -1,12 +1,30 @@
-import Meta from '../components/Meta';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import { Component } from "react";
+import { initGA, logPageView } from "../utils/analytics";
 
-export default ({ children }) => (
-  <div>
-		<Meta />
-		<Header />
-    {children}
-    <Footer />
-  </div>
-)
+import Meta from "../components/Meta";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+class Layout extends Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {children}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Layout;
