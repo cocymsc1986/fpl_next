401f88535bdc99a64402320c747b2fd23aa3b6d4
Michael Cockcroft <cocymsc@hotmail.com>
2019-08-09 12:35:38 +0100

    updating circle config with deploy

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+########################################
+# Put this on a Server
+# run chmod +x deploy_app.sh to make the script executable
+# 
+# Execute this script:  ./deploy_app.sh cocymsc/fpl_next:$TAG
+# Replace the $TAG with the actual Build Tag you want to deploy
+#
+########################################
+
+set -e
+
+DOCKER_IMAGE=$1
+CONTAINER_NAME="fpl_next"
+
+# Check for arguments
+if [[ $# -lt 1 ]] ; then
+    echo '[ERROR] You must supply a Docker Image to pull'
+    exit 1
+fi
+
+echo "Deploying fpl_next to Docker Container"
+
+#Check for running container & stop it before starting a new one
+if [ $(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME) = "true" ]; then
+    docker stop fpl_next
+fi
+
+echo "Starting FPL app using Docker Image name: $DOCKER_IMAGE"
+
+docker run -d --rm=true -p 3000:3000  --name fpl_next $DOCKER_IMAGE
+
+docker ps -a
